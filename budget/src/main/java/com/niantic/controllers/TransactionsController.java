2901d5e748b9dc3419a466b722bf1d5678fd1a3d
Karen Delarosa <kaydelarose@gmail.com>
2024-08-15 14:25:02 -0700

    created transaction controller
    created transaction/index.html

---
+++ budget/src/main/java/com/niantic/controllers/TransactionsController.java
@@ -0,0 +1,40 @@
+package com.niantic.controllers;
+
+import com.niantic.models.Category;
+import com.niantic.models.Transaction;
+import com.niantic.services.CategoryDao;
+import com.niantic.services.TransactionDao;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.ArrayList;
+
+
+@Controller
+public class TransactionsController
+{
+    TransactionDao transactionDao = new TransactionDao();
+
+
+    @GetMapping("/transactions")
+    public String getTransactions(Model model) {
+
+        ArrayList<Transaction> transactions;
+
+        transactions = transactionDao.getAllTransactions();
+
+        model.addAttribute("transactions", transactions);
+        return "transactions/index";
+    }
+
+
+
+
+
+
+}
+
